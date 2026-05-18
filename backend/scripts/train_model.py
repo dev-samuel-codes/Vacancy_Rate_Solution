ffcc2a5c47c674473a07e1f7845201a3bcf74759
Samuel <sksksjakskska@gmail.com>
2026-05-18 13:52:26 +0900

    feat: 학습 데이터 타깃 0/1로 정비

--- backend/scripts/train_model.py
+++ backend/scripts/train_model.py
@@ -5,10 +5,20 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import SGDClassifier
 
-#파일 불러오기
+# 파일 불러오기
 data = pd.read_csv("backend/data/survival_model_dataset.csv")
 
-#특성값과 타깃값 설정
+# 사용하지 않을 데이터 제거
+data = data.drop(columns=["period", "region"])
+
+# 문자열 범주형 데이터를 더미 변수로 변환
+data = pd.get_dummies(
+    data,
+    columns = ["district", "business_type"],
+    dtype = int
+)
+
+# 특성값과 타깃값 설정
 target_column = "target_survived_over_1_year"
 
 x = data.drop(columns=[target_column])
