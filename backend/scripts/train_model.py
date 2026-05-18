2dcebae5e4d59c78d11c027b396d6261ed24c63b
Samuel <sksksjakskska@gmail.com>
2026-05-18 13:40:56 +0900

    feat: 서울 생존 데이터 타깃 0/1로 수정

--- backend/scripts/train_model.py
+++ backend/scripts/train_model.py
@@ -1,9 +1,15 @@
-# 모델 학습해서 저장
-
 import keras
 
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import SGDClassifier
 
+#파일 불러오기
 data = pd.read_csv("backend/data/survival_model_dataset.csv")
+
+#특성값과 타깃값 설정
+target_column = "target_survived_over_1_year"
+
+x = data.drop(columns=[target_column])
+y = data[target_column]
